9e5864decf87b17210b6e89362ba6b747a6bd8d0
郑春娜 <596785154@qq.com>
2017-08-16 15:26:31 +0800

    first commit

---
+++ src/main/java/com/zcn/util/Person.java
@@ -0,0 +1,20 @@
+package com.zcn.util;
+
+/**
+ * Created by HTC on 2017/8/16.
+ */
+public class Person {
+    public int _id;
+    public String name;
+    public int age;
+    public String info;
+
+    public Person() {
+    }
+
+    public Person(String name, int age, String info) {
+        this.name = name;
+        this.age = age;
+        this.info = info;
+    }
+}
